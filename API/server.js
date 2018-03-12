e04d65a4ea6e8939bd65447888a369c4fad1a3af
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2018-03-12 15:12:55 -0300

    Tentando subir para heroku

--- API/server.js
+++ API/server.js
@@ -20,28 +20,31 @@ app.set("superSecret", config.secret);
 mongoose.connect(config.database);
 const db = mongoose.connection;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../build"));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(morgan("dev"));
-// //To prevent errors from Cross Origin Resource Sharing, we will set
-// //our headers to allow CORS with middleware like so:
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   //and remove cacheing so we get the most recent comments
-//   res.setHeader("Cache-Control", "no-cache");
-//   next();
-// });
+app.use(morgan("dev"));
+//To prevent errors from Cross Origin Resource Sharing, we will set
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  //and remove cacheing so we get the most recent comments
+  res.setHeader("Cache-Control", "no-cache");
+  next();
+});
 
 app.use(expressValidator());
 
